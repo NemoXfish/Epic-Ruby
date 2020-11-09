ee99e9df5bed5e1c0d385d5304e8337cfbe103f8
NemoXfish <nemodang2@gmail.com>
2020-11-09 12:58:40 -0800

    item tab liiiiiit

--- src/main/java/com/NemoXFish/cumlord/Cumlord.java
+++ src/main/java/com/NemoXFish/cumlord/Cumlord.java
@@ -1,6 +1,8 @@
 package com.NemoXFish.cumlord;
 
 import com.NemoXFish.cumlord.util.RegistryHandler;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -28,4 +30,11 @@ public class Cumlord {
 
     private void doClientStuff(final FMLClientSetupEvent event) {
     }
+
+    public static final ItemGroup TAB = new ItemGroup("cumlordtab") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(RegistryHandler.RUBY.get());
+        }
+    };
 }
