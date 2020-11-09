26408e90d6df53aa46c7b010852da13c6539cee2
NemoXfish <nemodang2@gmail.com>
2020-11-08 21:15:46 -0800

    Ah sheet, here we go again.

---
+++ src/main/java/com/NemoXFish/stuff/Stuff.java
@@ -0,0 +1,31 @@
+package com.NemoXFish.stuff;
+
+import com.NemoXFish.stuff.util.RegistryHandler;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("stuff")
+public class Stuff {
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "items";
+
+    public Stuff() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        RegistryHandler.init();
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+}
---
+++ src/main/java/com/NemoXFish/stuff/util/RegistryHandler.java
@@ -0,0 +1,20 @@
+package com.NemoXFish.stuff.util;
+
+import com.NemoXFish.stuff.Stuff;
+import com.NemoXFish.stuff.items.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+    public static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Stuff.MOD_ID);
+
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    //Items
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+}
